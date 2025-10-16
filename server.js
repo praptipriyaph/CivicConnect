74970b92a09dd6144e04ff5cbe472af2a2212ddb
AdityaSaxena17 <adityasaxena0017@gmail.com>
2025-10-16 09:12:41 +0530

    feat:add endpoit for complaint submission

--- server.js
+++ server.js
@@ -18,22 +18,23 @@ const supabase=createClient(process.env.SUPABASE_URL,process.env.SUPABASE_KEY)
 
 app.use(clerkMiddleware());
 
-app.get("/",requireAuth(),(req,res)=>{
-    // if(!req.auth.userId){
-    //     res.send("Failure!")
-    // }
-    res.send("Success!")
-})
+// app.get("/",requireAuth(),(req,res)=>{
+//     // if(!req.auth.userId){
+//     //     res.send("Failure!")
+//     // }
+//     res.send("Success!")
+// })
 
 app.post("/api/save-user",requireAuth(),async (req, res) => {
   try {
     console.log("API HIT")
     const user = req.body;
-    const exists= await supabase.from("users").select().eq("clerk_id",user.clerkid).maybeSingle();
-    if(exists.data){
+    const exists= await supabase.from("users").select().eq("clerk_id",user.clerk_id).maybeSingle();
+    if(!exists.data){
       const { data, error } = await supabase.from("users").insert([user]);
       if (error) throw error;
-      res.status(201).json(data.status);
+      console.log("USER ADDED!")
+      res.status(201).json(data);
     }
     else{
       res.status(200).send("User already exists!");
@@ -44,16 +45,25 @@ app.post("/api/save-user",requireAuth(),async (req, res) => {
   }
 });
 
-app.post("/api/raise-issue",(req,res)=>{
-    const user=req.auth.user
-    const issue=req.body
-
-
+app.post("/api/raise-issue",requireAuth(),async (req,res)=>{
+  try{
+    const clerk_id=req.auth.userId
+    const creator=await supabase.from("users").select("id").eq("clerk_id",clerk_id).single();
+    const {title,description,latitude,longitude,image} = req.body
+    const {data,error} = await supabase.from("issues").insert([{
+      title,description,latitude,longitude,image,created_by:creator.data.id
+    }])
+    if(error)throw error;
+    res.status(201).json(data)
+  }
+  catch(error){
+  res.status(500).json({ error: error.message })  
+  }
 })
 
-app.get("api/get-issues",async (req,res)=>{
+// app.get("api/get-issues",async (req,res)=>{
 
-})
+// })
 
 app.listen(PORT, () =>{
     console.log(`Server is running on port ${PORT}`);
